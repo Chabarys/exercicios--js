956880051286de4471341afb8f993bd7e581ab09
Chabarys <alissonchabaribery6@gmail.com>
2022-10-14 20:48:29 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,62 @@
+document.getElementById("titulo").innerHTML = "Opa, tudo bem?";
+document.write("algum texto")
+
+window.alert("menssagem de exemplo")
+
+console.log("a mensagem que eu inserir aqui, aparecerá no console.")
+
+// var idade = 90;
+// var nome = "Aline";
+
+var x = 10;
+var y = 15;
+
+var total = x + y;
+
+(alert(total))
+
+// var nome = "aline";
+var sobrenome = "chabaribery";
+
+var nomecompleto = nome+" "+sobrenome;
+
+(alert (nomecompleto))
+
+var hora = 9;
+
+if ( hora <12){
+    console.log("bom dia!")
+} else{
+    console.log("não de manhã!")
+}
+
+var hora = 21;
+
+if ( hora <12){
+    console.log("bom dia!")
+};
+
+if ( hora < 18){
+        console.log("bom dia!")
+    };
+
+
+if ( hora <= 23){
+        console.log("boa noite!")
+    };
+
+    var hora = 13;
+
+    if ( hora>= 12 && hora <18){
+        console.log("boa tarde!")
+    };
+
+
+    let nome = "pin";
+    let idade = 0;
+
+    if(nome == "pin"){
+        idade = 90
+    }
+        console.log(idade);
+    
